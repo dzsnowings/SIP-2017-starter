0699917f35880172db07f938ea29271b73317f02
dzsnowings <dzsnowings@gmail.com>
2017-06-13 10:41:51 -0400

    Create pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -11,7 +11,14 @@ y_pos = -150
 t.setposition(x_pos, y_pos)
 
 ### Write your code below:
-
+sides = input("Enter the number of sides: ")
+print("Your shape will have ", sides, ".")
+pendown()
+while(sides>0):
+  forward(100)
+  right(120)
+  sides -= 1
+penup()
 
 
 
